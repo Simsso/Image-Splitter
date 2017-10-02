4418e6efd7526f5a46aa7f33a8760a59769b8c81
Timo Denk <github@timodenk.com>
2017-10-02 12:29:01 +0200

    Image resizer

---
+++ ImageResizer/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+
+namespace ImageResizer
+{
+    /// <summary>
+    /// Resizes all images in a given input folder and stores them in the output folder.
+    /// </summary>
+    class Program
+    {
+        private static string inputPath = @"D:\Development\D2A\images\128",
+            outputPath = @"D:\Development\D2A\images\48";
+
+        private static Size targetSize = new Size(48, 48);
+
+        static void Main(string[] args)
+        {
+            string[] files = Directory.GetFiles(inputPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string filePath in files)
+            {
+                string fileName = Path.GetFileName(filePath);
+                try
+                {
+                    using (Bitmap input = (Bitmap)Bitmap.FromFile(filePath))
+                    {
+                        Bitmap output = new Bitmap(input, targetSize);
+                        output.Save(outputPath + @"\" + fileName);
+                        output.Dispose();
+                        Console.WriteLine(fileName + " was saved successfully.");
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine(filePath + " was skipped because an error occured.");
+                    continue;
+                }
+            }
+        }
+    }
+}
