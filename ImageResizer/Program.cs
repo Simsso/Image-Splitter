7145aa1bd1d4cace4b7b9c0ade8643daf3269f2d
Timo Denk <github@timodenk.com>
2017-10-03 21:12:43 +0200

    Label folders

--- ImageResizer/Program.cs
+++ ImageResizer/Program.cs
@@ -16,6 +16,12 @@ namespace ImageResizer
 
         static void Main(string[] args)
         {
+            Resize();
+            MoveIntoLabelFolders(.8f);
+        }
+
+        private static void Resize()
+        {
             string[] files = Directory.GetFiles(inputPath, "*", SearchOption.TopDirectoryOnly);
             foreach (string filePath in files)
             {
@@ -36,6 +42,59 @@ namespace ImageResizer
                     continue;
                 }
             }
+        }
+
+        private static void MoveIntoLabelFolders(float trainingRatio)
+        {
+            Random random = new Random();
+            string[] files = Directory.GetFiles(outputPath, "*", SearchOption.TopDirectoryOnly);
+            foreach (string filePath in files)
+            {
+                string label = Path.GetFileNameWithoutExtension(filePath).Split('_')[0];
+                string path = outputPath + @"\" + ((random.NextDouble() < trainingRatio) ? "train" : "test") + @"\" + LabelToFolderName(label);
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
+                File.Move(filePath, path + @"\" + Path.GetFileName(filePath));
+            }
+        }
+
+        private static string LabelToFolderName(string label)
+        {
+            int number;
+            char[] chars = label.ToLower().ToCharArray();
+            switch (chars[0])
+            {
+                case '2': number = 0; break;
+                case '3': number = 1; break;
+                case '4': number = 2; break;
+                case '5': number = 3; break;
+                case '6': number = 4; break;
+                case '7': number = 5; break;
+                case '8': number = 6; break;
+                case '9': number = 7; break;
+                case 't': number = 8; break;
+                case 'j': number = 9; break;
+                case 'q': number = 10; break;
+                case 'k': number = 11; break;
+                case 'a': number = 12; break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            switch (chars[1])
+            {
+                case 's': number += 13 * 0; break;
+                case 'd': number += 13 * 1; break;
+                case 'c': number += 13 * 2; break;
+                case 'h': number += 13 * 3; break;
+                default:
+                    throw new ArgumentException();
+            }
+
+            return number.ToString();
         }
     }
 }
